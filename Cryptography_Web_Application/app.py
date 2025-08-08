6ea58fd73383ca0acc4eaec50bd1e29ef899f022
zahidahm1 <zahidahm101@gmail.com>
2025-08-08 18:54:57 +0530

    added procfile, requirements and update app for deploying on render

--- Cryptography_Web_Application/app.py
+++ Cryptography_Web_Application/app.py
@@ -46,5 +46,10 @@ def encrypt():
 
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
